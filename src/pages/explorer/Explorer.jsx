3120ca66f4c3b7c3f070b464b90f15e3a25cf0ea
Amit Verma <vamit20000@gmail.com>
2024-06-16 00:10:57 +0530

    moviewebapp

---
+++ src/pages/explorer/Explorer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Explorer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Explorer
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroBanner from "./heroBanner/HeroBanner"
+
+const Home = () => {
+  return (
+    <div>
+      <HeroBanner/>
+    </div>
+  )
+}
+
+export default Home
